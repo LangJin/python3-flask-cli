bc72d9394fb7d8d1f4e6c02fac8c1bc0dd71bebc
LangJin <fenyukuang@163.com>
2022-04-28 01:53:47 +0800

    优化配置文件

--- gunicornConfig.py
+++ gunicornConfig.py
@@ -1,20 +1,19 @@
-# config.py
 import os
-import gevent.monkey
-gevent.monkey.patch_all()
-
 import multiprocessing
+from gevent import monkey
+monkey.patch_all()
 
-# debug = True
-loglevel = 'debug'
+
 bind = "0.0.0.0:2333"
+worker_class = 'gevent'
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = multiprocessing.cpu_count() * 2
+daemon = True
+loglevel = 'debug'
+x_forwarded_for_header = 'X-FORWARDED-FOR'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
 pidfile = "logs/gunicorn.pid"
 accesslog = "logs/access.log"
 errorlog = "logs/debug.log"
-daemon = True
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
 
-# 启动的进程数
-workers = multiprocessing.cpu_count()
-worker_class = 'gevent'
-x_forwarded_for_header = 'X-FORWARDED-FOR'
+
