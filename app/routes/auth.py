bf91e585aaddb8a3182deee9d04a897a707fc2d6
LangJin <fenyukuang@163.com>
2022-04-28 00:43:56 +0800

    测试

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,10 +3,9 @@ __author__ = 'LangJin'
 from . import authbp
 from flask import request
 from app.utils.dbfunc import MySQLDB as mysql
+from app.utils.responseFunc import resultMsg
 
-
-@authbp.route("/test",methods=["post"])
+@authbp.route("/test",methods=["get"])
 def regist():
-    body = request.get_json()
-    dbres = mysql().query("select * from t_user limit %s;",(1))
-    return body
+    dbres = mysql().query("selet * from t_user limit %s;",(1))
+    return resultMsg(data=dbres)
